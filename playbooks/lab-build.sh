15d95ef6d14e08b53f93b59a8250d46633a5d7e2
Stuart R. Kirk <stuart.kirk@microsoft.com>
2020-10-11 16:41:33 -0400

    Initial Repo Population

---
+++ playbooks/lab-build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# Build Script
+# Deploy and scale Microsoft Azure Cloud Native infrastructures and applications with Red Hat Ansible Automation
+# Written by Stuart Kirk <stuart.kirk@microsoft.com>
+
+# Init
+if [ -f "vars-myvars.yml" ]; then
+    echo "This script has already been run and your vars-myvars.yml file already exists."
+    exit 1
+fi
+
+# Variables
+RANDNUM="$(shuf -i 20000-50000 -n 1)"
+AZUREDC="$(az group list -o table |grep 01 | awk '{print $2}')"
+RG="01"
+
+# Inputs
+echo "Welcome to the Red Hat Summit 2020 lab Deploy and scale Microsoft Azure Cloud Native infrastructures and applications with Red Hat Ansible Automation"
+echo "Please answer the following questions so that we can get your lab environment set up and you can begin working on your lab exercises."
+echo " "
+echo -n "What is your first name:  > "
+read first
+echo -n "What is your last name:  > "
+read last
+echo -n "What year were you born:  > "
+read year
+echo -n "What is your GitHub ID:  > "
+read githubid
+echo -n "What is your GitHub Personal Access Token:  > "
+read gitpat
+echo -n "What is the Azure Red Hat OpenShift (ARO) API URL (i.e. https://api.xxx.yyy.aroapp.io:6443):  > "
+read aroapi
+
+echo " "
+
+# Validation
+echo "To Recap:"
+echo "Your first name is: $first"
+echo "Your last name is: $last"
+echo "You were born in: $year"
+echo "Your GitHub ID is: $githubid"
+echo "Your GitHub PAT is: $gitpat"
+echo "The ARO API URL is: $aroapi"
+
+echo " "
+
+echo "Is this correct?"
+PS3="Select a numbered option >> "
+options=("Yes" "No")
+select yn in "${options[@]}"
+do
+case $yn in
+    Yes ) break ;;
+    No ) echo "Please re-run the script and provide the correct values."; exit ;;
+esac
+done
+
+# Massage variables
+FIRSTLAST="${first}${last}${year}"
+VMNAME="$(echo $FIRSTLAST | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]//g' | cut -c 1-15)"
+AROFIRST="$(echo $first |cut -c 1)"
+ARONAME="${AROFIRST}${last}${RANDNUM}"
+USERNAME="$(echo $ARONAME | tr '[:upper:]' '[:lower:]')"
+
+# Operations
+cp vars.yml vars-myvars.yml
+sed -i "s/MYRANDOM/$RANDNUM/g" vars-myvars.yml
+sed -i "s/MY_RESOURCE_GROUP/$RG/g" vars-myvars.yml
+sed -i "s/MYGITHUBID/$githubid/g" vars-myvars.yml
+sed -i "s/MYGITHUBPERSONALACCESSTOKEN/$gitpat/g" vars-myvars.yml
+sed -i "s/MYVM/$VMNAME/g" vars-myvars.yml
+sed -i "s/MYAZUREDATACENTER/$AZUREDC/g" vars-myvars.yml
+sed -i "s/MYUSERNAME/$USERNAME/g" vars-myvars.yml
+sed -i "s+AROAPIURL+$aroapi+g" vars-myvars.yml
+
+# ARO Credentials
+
+echo " "
+echo -e "\e[1;41m ***********************************************************************************\e[0m"
+echo -e "\e[1;41m ***********************************************************************************\e[0m"
+echo -e "\e[1;41m ***********************************************************************************\e[0m"
+echo -e "\e[1;31m For the Azure Red Hat OpenShift (ARO) lab: \e[0m"
+echo -e "\e[1;31m Your username to log in to ARO is: $USERNAME \e[0m"
+echo -e "\e[1;31m Your password to log in to ARO is: Microsoft \e[0m"
+echo -e "\e[1;41m ***********************************************************************************\e[0m"
+echo -e "\e[1;41m ***********************************************************************************\e[0m"
+echo -e "\e[1;41m ***********************************************************************************\e[0m"
+curl --data "username=$USERNAME&password=Microsoft" https://wolverine.itscloudy.af/arolab.php
+
+# Finish
+echo " "
+echo "Your custom variables file, vars-myvars.yml, has been created.  Go forth and conquer!"
+exit 0
+
+
---
+++ provision-scripts/provision-script.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+AZ_USER_NAME=${1}
+AZ_USER_PASSWORD=${2}
+AZ_TENANT_ID=${3}
+AZ_SUBSCRIPTION_ID=${4}
+SP_NAME=${5}
+SP_SECRET=${6}
+SP_OBJECT_ID=${7}
+SP_APP_ID=${8}
+
+echo "`date` --BEGIN-- Provision Stage 1 Script" >>/root/provision-script-output.log
+echo "********************************************************************************************"
+	echo "`date` -- Setting Time Zone" >>/root/provision-script-output.log
+	echo "`date`" >>/root/provision-script-output.log
+	timedatectl set-timezone America/Detroit >>/root/provision-script-output.log
+	echo "`date`" >>/root/provision-script-output.log
+echo "********************************************************************************************"
+	echo "`date` -- Setting Student User password to 'Microsoft'" >>/root/provision-script-output.log
+	echo "Microsoft" | passwd --stdin student
+echo "********************************************************************************************"
+	echo "`date` -- Adding student to wheel group for sudo access'" >>/root/provision-script-output.log
+	usermod -G wheel student
+echo "********************************************************************************************"
+	echo "`date` -- Setting Root Password to 'Microsoft'" >>/root/provision-script-output.log
+	echo "Microsoft" | passwd --stdin root
+echo "********************************************************************************************"
+	echo "`date` -- Adding 'deltarpm' and other required RPMs" >>/root/provision-script-output.log
+        sed -i "s/=enforcing/=disabled/g" /etc/selinux/config
+        setenforce 0
+        echo "plugins=0" >> /etc/dnf/dnf.conf
+        sed -i "s/remove=True/remove=False/g" /etc/dnf/dnf.conf
+        echo "DRPM INSTALL" >> /root/yum-output.log
+        yum -y install drpm >> /root/yum-output.log
+        echo "PYTHON27 INSTALL" >> /root/yum-output.log
+        yum -y install @python27 >> /root/yum-output.log
+        echo "DEVELOPMENT INSTALL" >> /root/yum-output.log
+        yum -y install @development >> /root/yum-output.log
+        echo "REQUIRED RPM INSTALL" >> /root/yum-output.log
+        yum -y install python2-devel python2-pip libxslt-devel libffi-devel openssl-devel iptables arptables ebtables iptables-services telnet nodejs npm tigervnc-server tigervnc >> /root/yum-output.log
+        echo "SERVER W GUI INSTALL" >> /root/yum-output.log
+        yum -y group install "Server with GUI" -x file-roller* -x subscription-manager* -x dnf-plugin-subscription* -x libstoragemgmt* >> /root/yum-output.log
+        echo "REMOVE" >> /root/yum-output.log
+        yum -y remove rhn-check rhn-client-tools rhn-setup rhnlib rhnsd yum-rhn-plugin subscription-manager >> /root/yum-output.log
+        echo "FULL UPDATE" >> /root/yum-output.log
+        yum -y update >> /root/yum-output.log
+        echo "student ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+        alternatives --set python /usr/bin/python2
+        cd /usr/bin
+        curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+        chmod 755 /usr/bin/kubectl
+echo "********************************************************************************************"
+	echo "`date` -- Securing host and changing default SSH port to 2112" >>/root/provision-script-output.log
+	sed -i "s/dport 22/dport 2112/g" /etc/sysconfig/iptables
+	semanage port -a -t ssh_port_t -p tcp 2112
+	sed -i "s/#Port 22/Port 2112/g" /etc/ssh/sshd_config
+	systemctl restart sshd
+	systemctl stop firewalld
+	systemctl disable firewalld
+	systemctl mask firewalld
+	systemctl enable iptables
+	systemctl start iptables	
+echo "********************************************************************************************"
+        echo "`date` -- Upgrading PIP and installing Ansible" >>/root/provision-script-output.log
+        runuser -l student -c "pip-2.7 install --upgrade --user python-dateutil"
+        runuser -l student -c "pip-2.7 install --upgrade --user openshift"
+        runuser -l student -c "pip-2.7 install --upgrade --user requests"
+        runuser -l student -c "pip-2.7 install --upgrade --user xmltodict"
+        runuser -l student -c "pip-2.7 install --upgrade --user pyOpenSSL"
+        runuser -l student -c "pip-2.7 install --upgrade --user podman"
+        runuser -l student -c "pip-2.7 install --user ansible==2.9.10"
+        pip-2.7 install --upgrade selinux
+        find /usr/lib/python2.7/site-packages -type f -exec chmod 644 {} +
+        find /usr/lib/python2.7/site-packages -type d -exec chmod 755 {} +
+        echo "[defaults]" > /home/student/.ansible.cfg
+        echo "no_log = True" >> /home/student/.ansible.cfg
+        echo "[ssh_connection]" >> /home/student/.ansible.cfg
+        echo "ssh_args = -o StrictHostKeyChecking=no" >> /home/student/.ansible.cfg
+        chown student:student /home/student/.ansible.cfg
+echo "********************************************************************************************"	
+	echo "`date` -- Installing the Azure Linux CLI" >>/root/provision-script-output.log
+	rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+	yum -y install azure-cli >> /root/yum-output.log
+echo "********************************************************************************************"	
+	echo "`date` -- Setting default systemd target to graphical.target" >>/root/provision-script-output.log
+	systemctl set-default graphical.target >> /root/provision-script-output.log
+echo "********************************************************************************************"
+	echo "`date` -- Installing noVNC environment" >>/root/provision-script-output.log
+        pip-2.7 install numpy websockify
+        chmod -R a+rx /usr/lib64/python2.7/site-packages/numpy*
+        chmod -R a+rx /usr/lib/python2.7/site-packages/websockify*
+        wget --quiet -P /usr/local https://github.com/novnc/noVNC/archive/v1.1.0.tar.gz
+        cd /usr/local
+        tar xvfz v1.1.0.tar.gz
+        ln -s /usr/local/noVNC-1.1.0/vnc.html /usr/local/noVNC-1.1.0/index.html
+        wget --quiet -P /etc/systemd/system https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/websockify.service
+	wget --quiet --no-check-certificate -P /etc/systemd/system "https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/vncserver@:4.service"
+	openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/novnc.pem -out /etc/pki/tls/certs/novnc.pem -days 365 -subj "/C=US/ST=Michigan/L=Ann Arbor/O=RedHatSummit/OU=CloudNativeAzure/CN=microsoft.com"
+	su -c "mkdir .vnc" - student
+	wget --quiet --no-check-certificate -P /home/student/.vnc https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/passwd
+	wget --quiet --no-check-certificate -P /home/student/.vnc https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/xstartup
+        chown student:student /home/student/.vnc/passwd
+        chown student:student /home/student/.vnc/xstartup
+        chmod 600 /home/student/.vnc/passwd
+        chmod 755 /home/student/.vnc/xstartup
+	iptables -I INPUT 1 -m tcp -p tcp --dport 6080 -j ACCEPT
+	service iptables save
+        chmod 644 /etc/systemd/system/pm2-root.service
+        chmod 644 /etc/systemd/system/vncserver@\:4.service
+        chmod 644 /etc/systemd/system/websockify.service
+        systemctl daemon-reload
+        systemctl enable vncserver@:4.service
+        systemctl enable websockify.service
+        systemctl start vncserver@:4.service
+	systemctl start websockify.service
+echo "********************************************************************************************"
+	echo "`date` -- Editing student's .bashrc and disabling Red Hat alerts" >> /root/provision-script-output.log
+	echo " " >> /home/student/.bashrc
+        echo "# Azure Service Principal Credentials" >> /home/student/.bashrc
+	echo "export AZURE_CLIENT_ID=$SP_APP_ID" >> /home/student/.bashrc
+	echo "export AZURE_SECRET=$SP_SECRET" >> /home/student/.bashrc
+	echo "export AZURE_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID" >> /home/student/.bashrc
+	echo "export AZURE_TENANT=$AZ_TENANT_ID" >> /home/student/.bashrc
+        su -c "gconftool-2 -t bool -s /apps/rhsm-icon/hide_icon true" - student
+	su -c "ssh-keygen -t rsa -q -P '' -f /home/student/.ssh/id_rsa" - student
+        mkdir -p /home/student/.local/share/keyrings
+	wget --quiet -P /home/student/.local/share/keyrings https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/Default.keyring
+        chown student:student /home/student/.local/share/keyrings/Default.keyring
+        restorecon /home/student/.local/share/keyrings/Default.keyring
+echo "********************************************************************************************"
+        wget -P /etc/yum.repos.d https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/mongodb-org-4.2.repo 
+        yum -y install mongodb-org 
+        npm install pm2@latest -g
+        systemctl enable mongod
+        systemctl start mongod
+        iptables -I INPUT 2 -m tcp -p tcp --dport 80 -j ACCEPT
+        export MONGO_DBCONNECTION="mongodb://localhost:27017/nodejs-todo"
+        mkdir -p /source/sample-apps/nodejs-todo/src
+        cd /source/sample-apps/nodejs-todo/src   
+        git clone https://github.com/stuartatmicrosoft/nodejs-todo .
+        chown -R student:student /source
+        npm install
+        sed -i "s/8080/80/g" /source/sample-apps/nodejs-todo/src/server.js
+        pm2 start server.js
+        pm2 save
+        pm2 startup systemd -u root
+        systemctl start pm2-root
+        service iptables save
+echo "********************************************************************************************"
+        chown -R student:student /home/student/.local
+        chmod a+rx /home/student/.local
+        cd /usr/local/bin
+        wget -P /usr/local/bin https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/oc.tar.gz
+        tar xvfz oc.tar.gz
+        rm -f oc.tar.gz
+echo "********************************************************************************************"
+        echo "`date` -- Installing MSSQL Tools" >>/root/provision-script-output.log
+        curl https://packages.microsoft.com/config/rhel/8/prod.repo > /etc/yum.repos.d/msprod.repo
+        ACCEPT_EULA=Y yum install -y mssql-tools unixODBC-devel
+        echo "PATH=$PATH:/opt/mssql-tools/bin" >> /etc/profile
+echo "********************************************************************************************"
+
+echo "`date` --END-- Provisioning" >>/root/provision-script-output.log
+
+echo "`date` Creating Student Desktop Credentials File" >>/root/provision-script-output.log
+
+mkdir /home/student/Desktop
+chown student:student /home/student/Desktop
+echo AZURE_USER_NAME=$AZ_USER_NAME >> /home/student/Desktop/credentials.txt
+echo AZURE_USER_PASSWORD=$AZ_USER_PASSWORD >> /home/student/Desktop/credentials.txt
+echo AZURE_CLIENT_ID=$SP_APP_ID >> /home/student/Desktop/credentials.txt
+echo AZURE_SECRET=$SP_SECRET >> /home/student/Desktop/credentials.txt
+echo AZURE_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID >> /home/student/Desktop/credentials.txt
+echo AZURE_TENANT_ID=$AZ_TENANT_ID >> /home/student/Desktop/credentials.txt
+echo GUIDE_URL=https://github.com/stuartatmicrosoft/RedHatSummit2020 >> /home/student/Desktop/credentials.txt
+chown student:student /home/student/Desktop/credentials.txt
+
+echo "`date` --END-- Provision Script" >>/root/provision-script-output.log
+
