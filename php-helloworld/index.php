fba89590f2c458cc905088ac7fd7dd520f3d7350
Stuart R. Kirk <stuart.kirk@microsoft.com>
2020-10-13 02:27:05 -0400

    Summit to Ansiblefest Updates

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
 print "<h1 align=middle>Hello there!<br><br>";
-print "Welcome to Azure Red Hat OpenShift!!<br><br>";
+print "Welcome to Ansiblefest 2020 & Azure Red Hat OpenShift!!<br><br>";
 print "<img align=middle src=https://raw.githubusercontent.com/stuartatmicrosoft/Ansiblefest2020/main/images/ansiblefest2020.png><br><br>";
 print "Your php version is " . PHP_VERSION . "\n";
 ?>
